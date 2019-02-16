099850d81c35c59e3e22d4c36666cbaf897e99f7
Nick Ebert <nick.ebert@yahoo.com>
2019-02-16 13:58:40 -0600

    Added function for creating array of points from two vectors

--- utils.py
+++ utils.py
@@ -5,3 +5,12 @@ class Point:
         
     def __repr__(self):
         return '({0}, {1})'.format(self.x, self.y)
+
+def create_point_array(x_vec, y_vec):
+    assert len(x_vec) == len(y_vec), "Lists not same length"
+
+    points = []
+    for (x, y) in zip(x_vec, y_vec):
+        points.append(Point(x, y))
+
+    return points
