4dd9581e7b5adcad811903c24c42e2eac5242586
Nick Ebert <nick.ebert@yahoo.com>
2019-02-16 17:46:19 -0600

    Updated utils.py to support sample with various measurements, pos, vel, acc, timestamp. Updated vis.py to support these changes

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
-class Point:
+import random
+
+class Vec2d:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -6,11 +8,46 @@ class Point:
     def __repr__(self):
         return '({0}, {1})'.format(self.x, self.y)
 
-def create_point_array(x_vec, y_vec):
+    @classmethod
+    def rand(self):
+        return Vec2d(random.random(), random.random())
+
+SAMPLE_ATTRIBUTES = ('pos', 'vel', 'acc', 'time')
+
+class Sample:
+    def __init__(self, pos, vel, acc, time):
+        self.pos = pos
+        self.vel = vel
+        self.acc = acc
+        self.time = time
+
+    def __repr__(self):
+        return 'Sample{{ pos{0}, vel{1}, acc{2}, time({3}) }}' \
+            .format(self.pos.__repr__(), \
+            self.vel.__repr__(), \
+            self.acc.__repr__(), \
+            self.time)
+
+    @classmethod
+    def rand(self):
+        return Sample(Vec2d.rand(), Vec2d.rand(), Vec2d.rand(), random.random())
+
+def create_vec2d_array(x_vec, y_vec):
     assert len(x_vec) == len(y_vec), "Lists not same length"
 
     points = []
     for (x, y) in zip(x_vec, y_vec):
-        points.append(Point(x, y))
+        points.append(Vec2d(x, y))
 
     return points
+
+if __name__ == '__main__':
+    p = Vec2d(0, 1)
+    v = Vec2d(2, 3)
+    a = Vec2d(4, 5)
+    t = 0.5
+
+    s = Sample(p, v, a, t)
+    print(s)
+
+    print(Sample.rand())
---
+++ vis.py
@@ -0,0 +1,40 @@
+import utils
+import random
+import matplotlib.pyplot as plt
+
+class SampleVisualizer:
+    def __init__(self, samples=[], rand_sample_len=None):
+        self.samples = samples
+
+        if rand_sample_len is not None:
+            self.samples = self.gen_rand_samples(rand_sample_len)
+
+    def plot_meas(self, meas):
+        assert meas in utils.SAMPLE_ATTRIBUTES, "Measurement doesn't exist"
+        assert type(getattr(self.samples[0], meas)) == utils.Vec2d, "Must use Vec2d type measurement"
+
+        x = [getattr(v, meas).x for v in self.samples]
+        y = [getattr(v, meas).y for v in self.samples]
+        
+        plt.plot(x, y, 'ro')
+
+    def gen_rand_samples(self, rand_sample_len):
+        samp = []
+        for i in range(rand_sample_len):
+            x_rand = random.random()
+            y_rand = random.random()
+
+            s = utils.Vec2d(x_rand, y_rand)
+
+            samp.append(utils.Sample(s, s, s, 10))
+
+        return samp
+        
+if __name__ == '__main__':
+    v = SampleVisualizer(rand_sample_len=400)
+
+    v.plot_meas('pos')
+    plt.show()
+
+
+
