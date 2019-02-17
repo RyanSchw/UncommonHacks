09251c488e2882bcddae3014438f8210e8f3399a
Ryan Schwartz <schwar95@purdue.edu>
2019-02-17 03:09:33 -0600

    Add lap testing
    Lap has tests for both Checkpoint and Run

--- tests/test-lap.py
+++ tests/test-lap.py
@@ -1,7 +1,6 @@
 import unittest
-from Checkpoint import Checkpoint
-import Vec2d
-import Sample
+from Lap import Checkpoint, Run
+from utils import Vec2d, Sample
 
 class TestCheckpoint(unittest.TestCase):
     def test_intersection(self):
@@ -74,6 +73,51 @@ class TestCheckpoint(unittest.TestCase):
 
         self.assertTrue(checkpoint.crossedCheckpoint(sample1, sample2))
 
+class TestRun(unittest.TestCase):
+    def test_funtionality(self):
+        path = [
+            Sample(
+                Vec2d(0, 0),
+                Vec2d(0, 0), Vec2d(0, 0), 0
+            ),
+            Sample(
+                Vec2d(1, 1),
+                Vec2d(0, 0), Vec2d(0, 0), 1
+            ),
+            Sample(
+                Vec2d(2, 0),
+                Vec2d(0, 0), Vec2d(0, 0), 2
+            ),
+            Sample(
+                Vec2d(3, 3),
+                Vec2d(0, 0), Vec2d(0, 0), 3
+            ),
+            Sample(
+                Vec2d(4, 4),
+                Vec2d(0, 0), Vec2d(0, 0), 4
+            ),
+            Sample(
+                Vec2d(-1, 0),
+                Vec2d(0, 0), Vec2d(0, 0), 5
+            )
+        ]
+        checkpoints = [
+            Checkpoint(
+                1,
+                Vec2d(1, 0),
+                Vec2d(1, 2)
+            ),
+            Checkpoint(
+                2,
+                Vec2d(2.5, 0),
+                Vec2d(2.5, 3)
+            )
+        ]
+        lap = Run(path, checkpoints)
+        print(lap)
+
 # Display all the tests passing
-suite = unittest.TestLoader().loadTestsFromTestCase(TestCheckpoint)
+# suite = unittest.TestLoader().loadTestsFromTestCase(TestCheckpoint)
+# unittest.TextTestRunner(verbosity=2).run(suite)
+suite = unittest.TestLoader().loadTestsFromTestCase(TestRun)
 unittest.TextTestRunner(verbosity=2).run(suite)
